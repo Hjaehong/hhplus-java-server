57edcea1a1903170813bbe106bda795ccdb36c4f
Hjaehong <zero5140@naver.com>
2025-06-06 02:14:39 +0900

    대기열의 상태를 확인하기위한 클래스 추가

---
+++ src/main/java/kr/hhplus/be/server/layer/wait/controller/WaitController.java
@@ -0,0 +1,4 @@
+package kr.hhplus.be.server.layer.seat.controller;
+
+public class SeatController {
+}
---
+++ src/main/java/kr/hhplus/be/server/layer/wait/repository/WaitRepository.java
@@ -0,0 +1,4 @@
+package kr.hhplus.be.server.layer.seat.repository;
+
+public class SeatRepository {
+}

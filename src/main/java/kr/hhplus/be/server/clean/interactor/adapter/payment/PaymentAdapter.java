35ced898a9440f0adebabe968299b1a523ff40f0
Hjaehong <zero5140@naver.com>
2025-06-06 16:19:17 +0900

    결재, 예약의 웹 & db를 바라보는 adapter

---
+++ src/main/java/kr/hhplus/be/server/clean/interactor/adapter/payment/PaymentAdapter.java
@@ -0,0 +1,4 @@
+package kr.hhplus.be.server.clean.interactor.adapter.payment;
+
+public class PaymentAdapter {
+}
---
+++ src/main/java/kr/hhplus/be/server/clean/interactor/adapter/reservation/ReservationAdapter.java
@@ -0,0 +1,4 @@
+package kr.hhplus.be.server.clean.interactor.adapter.reservation;
+
+public class ReservationAdapter {
+}
